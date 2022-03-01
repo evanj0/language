a6603cc1597d4643cdd53a031d240718bc751ea2
evanj0nes <48502664+evanj0@users.noreply.github.com>
2022-02-28 17:48:57 -0800

    stuff

--- test/Typing.Tests/Inference_Tests.fs
+++ test/Typing.Tests/Inference_Tests.fs
@@ -3,10 +3,9 @@ module Inference.Tests
 open ResultExtensions
 open Range
 open Ir
-
 open NUnit.Framework
 
-let tryTest expr t =
+let tryInfer expr =
     result {
         let pos = { Position.column = 0; line = 0; index = 0 }
         let env = { Env.currentRange = Range.create pos pos; globals = []; locals = []; }
@@ -14,8 +13,19 @@ let tryTest expr t =
             Inference.infer expr { State.index = 0 } (fun t env -> []) env
             |> IResult.toResult
             |> Result.map (fun (_state, t, _cs) -> t)
-        return t |> Type.equals resultT
+        return resultT
     }
 
-[<Test>]
-let test1() = Assert.Fail()
+let test expr t =
+    match tryInfer expr with
+    | Ok resultT -> 
+        printfn "Expected: %s" (Type.print t)
+        printfn "Got: %s" (Type.print resultT)
+        Assert.True(t |> Type.equals resultT)
+    | Error e -> Assert.Fail(sprintf "Message: %s\nNote: %s\nRange: %s\n" e.message e.note e.range.Display)
+
+[<TestFixture>]
+module Literals =
+    open UntypedIr
+    [<Test>]
+    let ``string literal``() = test (Expr.Literal (Literal.Str "string literal")) (Type.Primitive Type.Str)
